dfa6b67c6d73b56b171798ff251ea730c57b6a4c
Bharath Subramanian <bharath.1581995@gmail.com>
2018-10-31 12:06:04 -0400

    Update windows.c
    Traverse the VAD and store it in a linked list

--- src/process/windows.c
+++ src/process/windows.c
@@ -21,7 +21,7 @@
  */
 
 #include <stdio.h>
-
+#include <stdlib.h>
 #include <libvmi/libvmi.h>
 #include <libvmi/events.h>
 
@@ -255,3 +255,94 @@ mem_seg_t vmi_current_find_segment_windows(vmi_instance_t vmi, vmi_event_t *even
 
     return mem_seg;
 }
+
+GSList* traverse(vmi_instance_t vmi, GSList* list, addr_t curr_vad){
+ 
+ if(curr_vad == 0){
+         return list;
+       }
+ addr_t startingvpn;
+ addr_t endingvpn;
+ addr_t mmvad_endingvpn = curr_vad + process_vmi_windows_rekall.mmvad_endingvpn;
+ addr_t mmvad_startingvpn = curr_vad + process_vmi_windows_rekall.mmvad_startingvpn;
+
+ if (vmi_read_addr_va(vmi, mmvad_startingvpn, 0, &startingvpn) != VMI_SUCCESS){
+     return list;}
+ startingvpn <<= 12; // virtual page number << 12 (4KB per page) = virtual address
+
+ if (vmi_read_addr_va(vmi, mmvad_endingvpn, 0, &endingvpn) != VMI_SUCCESS){
+      return list;}
+ endingvpn <<= 12; // virtual page number << 12 (4KB per page) = virtual address
+
+ mem_seg_t *mem = (mem_seg_t *)malloc(sizeof(mem_seg_t));
+
+ if(startingvpn != 0 && ((startingvpn>>63)==0) && (startingvpn<endingvpn) && ((endingvpn>>63)==0)){
+   printf("starting_vpn: 0x%lx\n",startingvpn);
+   printf("ending_vpn: 0x%lx\n",endingvpn);
+   mem->base_va = startingvpn;
+   mem->size = endingvpn - startingvpn;
+   printf("base_va: 0x%lx\n",mem->base_va);
+   printf("size: 0x%lx\n",mem->size);
+   list = g_slist_append(list, mem);
+ }
+  
+ //traversing left child in preorder traversal
+ addr_t mmvad_next_left = curr_vad + process_vmi_windows_rekall.mmvad_leftchild;
+ addr_t next_left;
+ if (vmi_read_addr_va(vmi, mmvad_next_left, 0, &next_left) != VMI_SUCCESS){
+      return list;}
+ list = traverse(vmi, list, next_left);
+  
+ //traversing right child in preorder traversal 
+ addr_t mmvad_next_right = curr_vad + process_vmi_windows_rekall.mmvad_rightchild;
+ addr_t next_right;
+ if (vmi_read_addr_va(vmi, mmvad_next_right, 0, &next_right) != VMI_SUCCESS){
+      return list;}
+ list = traverse(vmi, list, next_right);     
+
+ return list; 
+}
+
+GSList* vmi_current_vad_list_windows(vmi_instance_t vmi, vmi_event_t *event, addr_t addr)
+{
+    mem_seg_t mem_seg =
+    {
+        mem_seg.base_va = 0,
+        mem_seg.size = 0,
+    };
+    
+    GSList* list = NULL;
+
+    if (!process_vmi_ready)
+    {
+        fprintf(stderr, "ERROR: Windows Process VMI - Not initialized\n");
+        return list;
+    }
+
+    addr_t process = vmi_current_process_windows(vmi, event);
+
+    if (!process)
+    {
+        fprintf(stderr, "WARNING: Windows Process VMI - Could not find current process\n");
+        return list;
+    }
+
+    // Windows tracks memory mappings using Virtual Address Descriptors (VADs), which serves the
+    // same purpose of Linux's virtual memory areas (VMAs). VADs are organized as a balanced binary
+    // tree starting with the root VAD.
+    //
+    // For more information, see: http://lilxam.tuxfamily.org/blog/?p=326&lang=en
+    addr_t curr_vad = 0;
+    addr_t eprocess_vadroot = process + process_vmi_windows_rekall.eprocess_vadroot;
+
+    if (vmi_read_addr_va(vmi, eprocess_vadroot, 0, &curr_vad) != VMI_SUCCESS)
+    {
+        fprintf(stderr, "WARNING: Windows Process VMI - Could not find root VAD\n");
+        return list;
+    }
+    // root VAD is an _EX_FAST_REF, so the 3 least significant bits are a reference counter.
+    curr_vad &= 0xFFFFFFFFFFFFFFF8;
+    list = traverse(vmi, list, curr_vad);
+    return list;
+}
+
